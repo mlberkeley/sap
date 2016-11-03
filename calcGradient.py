05233b4ccbec0225d21ac8ba3cc9e572d592b3d9
CSSachse <christoph.sachse@gmail.com>
2016-11-03 10:46:05 -0700

    cleaned up syntax

--- calcGradient.py
+++ calcGradient.py
@@ -6,9 +6,9 @@
 # part 5
 
 # helper function =2u(t)-1 where u is heaviside step
-heavi2 = lambda x: 1 if x>0 else -1
+sgn = lambda x: 1 if x>0 else -1
 
-# These values are needed nowhere else; i'm not going to bother computing them
+# These values are needed nowhere else; we don't need to bother computing them
 
 #dR_tdf_t = heavi2(f[t-1]-f[t])
 #dR_tdf_t_1 = close[t] - open[t] -dR_tdf_t
@@ -17,4 +17,4 @@ heavi2 = lambda x: 1 if x>0 else -1
 #dU_tdtheta=sum((dR_tdf_t)*(df_tdtheta) + (dR_tdf_t_1)*(df_t_1dtheta)))
 
 # $\frac{dU_T}{d\theta}=\sum_{t=1}^T \frac{dU_T}{dR_t} ((\frac{dR_t}{df_t})*(\frac{df_t}{d\theta}) + (\frac{dR_t}{df_{t-1}})*(\frac{df_{t-1}}{dtheta})) $
-dUdtheta = dataframe2.map(lambda x: x['dU[i]/dR[i]']*(heavi2(x['f[i-1]']-x['f[i]'])*x['D[i]'] + (x['close[i]']-x['open[i]']-heavi2(x['f[i-1]']-x['f[i]']))*x['D[i-1]']))
+dUdtheta = dataframe2.map(lambda x: x['dU[i]/dR[i]']*(sgn(x['f[i-1]']-x['f[i]'])*x['D[i]'] + (x['close[i]']-x['open[i]']-sgn(x['f[i-1]']-x['f[i]']))*x['D[i-1]']))
