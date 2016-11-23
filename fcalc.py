a0f77c2e6b04016d7f598f86cc8166187f3ddd29
Zhongxia Yan <zxyan@berkeley.edu>
2016-11-22 22:06:44 -0800

    fixed calcgradient and fcalc

--- fcalc.py
+++ fcalc.py
@@ -2,16 +2,12 @@
 Tested separately. Still receiving problems with lag()
 '''
 from pyspark import SparkContext
-from pyspark.sql import SQLContext
-from pyspark.sql.functions import lag, pow, col
+from pyspark.sql import SQLContext, functions as F
 from pyspark.sql.window import Window
-from scan_reward import scan
+import sys
 
 sc = SparkContext()
 sqlContext = SQLContext(sc)
-################
-# PLACEHOLDERS #
-################
 
 # theta4 := scalar value for last parameter
 # fprime := scalar containing dot productg
@@ -21,20 +17,16 @@ fprime = -3
 theta4 = 10
 
 # range_df := dataframe containing [0, n]
-range_df = sqlContext.range(n+1)
+range_df = sqlContext.range(n + 1).select(F.col('id').alias('index'))
 
-#$\theta_4^f$:
-theta4f = range_df.select(pow(theta4, n - col('id')).alias('value'))
-
-#$\theta_4^r$:
-theta4r = range_df.select(pow(theta4, col('id') - n).alias('value'))
+#$\theta_4^r
+#$\theta_4^f
+theta4fr = range_df.select('index', F.pow(theta4, n - F.col('index')).alias('f'), F.pow(theta4, F.col('index') - n).alias('r'))
 
 # moving average F ; assumes scan is implemented
-Fhat_temp = scan(theta4f.select(fprime * col('value')))
-
-# assumes that theta4f and all those have a value column name
-Fhat = Fhat_temp.join(theta4r).select((Fhat_temp.value * theta4r.value).alias('value'))
+w_scan = Window.orderBy('index').rangeBetween(-sys.maxsize, 0)
+Fhat = theta4fr.select('index', (fprime * F.sum('f').over(w_scan) * F.col('r')).alias('Fhat'))
 
 # take the difference between the value and previous value
-w = Window.orderBy('index')
-F = Fhat.select(Fhat.value - lag("value"))
+w_lag = Window.orderBy('index').rowsBetween(-1, -1)
+Fdif = Fhat.select('index', F.col('Fhat') - F.lag('Fhat', default=0).over(w_lag))
