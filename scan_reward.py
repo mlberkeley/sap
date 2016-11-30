89b51bfb1cc4f08001306c68cf3868eb51625183
Gan Tu <tugan@berkeley.edu>
2016-11-30 02:51:15 -0800

    fix wrong syntax within abs value

--- scan_reward.py
+++ scan_reward.py
@@ -19,7 +19,7 @@ def reward(data, dfF, delta=0.1):
     df = data.join(dfF, F.col('data.index') == F.col('dfF.index')).select(F.col('dfF.index').alias('index'),
         ((F.col('data.close') - F.col('data.open')) \
         * F.col('dfF.lagF') \
-        - delta * F.abs(F.col('dfF.lagF'))).alias('R'))
+        - delta * F.abs(F.col('dfF.F') - F.col('dfF.lagF'))).alias('R'))
     return df
 
 def test():
