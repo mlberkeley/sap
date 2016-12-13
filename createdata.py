f2d8c3162ebcf359c1047a834ee6e824570475f1
philkuz <superpker@gmail.com>
2016-12-12 19:21:44 -0800

    Remove absolute default file path and update table query for createdata.py Removed the unnecessary absolute default file path and updated the sql query to get data from the table

--- createdata.py
+++ createdata.py
@@ -4,26 +4,27 @@ import sqlite3
 
 def load_data(sqlContext, file_path=None):
     if file_path is None:
-        file_path = "/Users/philkuz/projects/mlab/sap/StockData.db"
+        file_path = "StockData.db"
 
     conn = sqlite3.connect(file_path)
     c = conn.cursor()
     # c.execute("Create Index on StockData")
-    # result = c.execute("SELECT ROW_NUMBER() OVER(ORDER BY Date) AS RowNumber, Close - Open as D0, Low as D1, High as D2, Volume as D3 FROM StockData WHERE Symbol == AA")
-    result = c.execute("SELECT id, Close - Open as D0, Low as D1, High as D2, Volume as D3 FROM StockData WHERE Symbol='AA' LIMIT 200")
+    # result = c.execute("CREATE TABLE aaData AS SELECT sd.id, sd.Close - sd.Open as D0, sd.Low as D1, sd.High as D2, sd.Volume as D3 FROM StockData sd WHERE sd.Symbol='AA'  ORDER BY date(Date) DESC")
+    # result = c.execute("SELECT id, Close - Open as D0, Low as D1, High as D2, Volume as D3 FROM StockData WHERE Symbol='AA' ORDER BY date(Date) DESC")
+    result = c.execute("SELECT * from aaData")
     data = [r for r in result]
 
     df = sqlContext.createDataFrame(data, ['index', 'close-open', 'low', 'high', 'volume'])
     return df
 def test():
-    # note that not passing a parameter will default to Phillip's hard coding
-    load_data().show()
-
-if __name__ == "__main__":
     from pyspark import SparkContext
     from pyspark.sql import SQLContext
 
     sc = SparkContext()
     sqlContext = SQLContext(sc)
+    # note that not passing a filename for the database will default to Phillip's hard coding
+    load_data(sqlContext).show()
+
+if __name__ == "__main__":
 
     test()
